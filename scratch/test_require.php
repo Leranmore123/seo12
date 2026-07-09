c719454b8bd800d1e40c57803843d92695771b49
kanzariyapratik007 <kanzariyapratik124@gmail.com>
2026-07-09 10:37:48 +0530

    Update scratch/test_require.php session mock

--- scratch/test_require.php
+++ scratch/test_require.php
@@ -3,4 +3,8 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
+session_start();
+$_SESSION['user_id'] = 1;
+$_SESSION['role'] = 'admin';
+
 require_once dirname(__DIR__) . '/submission-manager.php';
