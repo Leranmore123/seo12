13bff8cc27b603d0d897023736723b82731c53dc
kanzariyapratik007 <kanzariyapratik124@gmail.com>
2026-08-06 00:37:21 +0530

    Test blog identifier variations for Tumblr API

--- scratch/test_tumblr_debug.php
+++ scratch/test_tumblr_debug.php
@@ -1,17 +1,43 @@
 <?php
 require_once __DIR__ . '/../config.php';
+require_once __DIR__ . '/../auto-poster.php';
 
-$db = getDB();
+$consumerKey = 'hPzVCCOxhVXN2nkRljbQa45yTvmkbD0ORiJ0N2uyA8iwJGhwcS';
+$consumerSecret = 'i4V1CBC22FU977dJgydgOcVRIlPCtSuBvre3bGdv41VAKsAXRZ';
+$oauthToken = 'bu9GtwLSMCC4SQckhzWvRvrplHwTCtwFxSR2ytHOB8EEhBAgHQ';
+$oauthTokenSecret = 'rflb0Fbhikw5AvcM6YmK2jbwbFnQgQiu1gLR0ymXw1FbB9dw6C';
 
-echo "=== Projects Table Columns & Sample Data ===\n";
-$stmt = $db->query("SELECT * FROM projects LIMIT 10");
-$projects = $stmt->fetchAll(PDO::FETCH_ASSOC);
+echo "=== Testing Tumblr API Post with Blog Identifier Variations ===\n\n";
 
-if (!empty($projects)) {
-    echo "Columns: " . implode(', ', array_keys($projects[0])) . "\n\n";
-    foreach ($projects as $p) {
-        print_r($p);
-    }
-} else {
-    echo "Projects table is empty!\n";
+$identifiers = [
+    'propertiesdelersblog.tumblr.com',
+    'propertiesdelersblog'
+];
+
+foreach ($identifiers as $blogId) {
+    $url = "https://api.tumblr.com/v2/blog/{$blogId}/post";
+    $postFields = [
+        'type'  => 'text',
+        'title' => 'Test Title ' . time(),
+        'body'  => "Test Body Content\nLine 2",
+        'tags'  => 'test,realestate',
+    ];
+    
+    $authHeader = getTumblrOAuthHeader($consumerKey, $consumerSecret, $oauthToken, $oauthTokenSecret, $url, 'POST', $postFields);
+    
+    $ch = curl_init($url);
+    curl_setopt_array($ch, [
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_POST           => true,
+        CURLOPT_POSTFIELDS     => http_build_query($postFields, '', '&', PHP_QUERY_RFC3986),
+        CURLOPT_HTTPHEADER     => [$authHeader, 'Content-Type: application/x-www-form-urlencoded'],
+        CURLOPT_TIMEOUT        => 10,
+        CURLOPT_SSL_VERIFYPEER => false,
+    ]);
+    $resp = curl_exec($ch);
+    $code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+    curl_close($ch);
+    
+    echo "Blog ID: '{$blogId}' => HTTP {$code}\n";
+    echo "Response: " . substr($resp, 0, 150) . "\n----------------------------------------\n";
 }
