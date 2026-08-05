c14108bd2c633aaaf4df66c59a12b5e44d2e1266
kanzariyapratik007 <kanzariyapratik124@gmail.com>
2026-08-06 00:40:56 +0530

    Check base64 decoding of Tumblr tokens

--- scratch/test_tumblr_debug.php
+++ scratch/test_tumblr_debug.php
@@ -1,28 +1,12 @@
 <?php
 require_once __DIR__ . '/../config.php';
-require_once __DIR__ . '/../auto-poster.php';
 
-echo "=== Waiting 5 seconds to clear Tumblr rate limit... ===\n";
-sleep(5);
+$tok = 'bu9GtwLSMCC4SQckhzWvRvrplHwTCtwFxSR2ytHOB8EEhBAgHQ';
+$sec = 'rflb0Fbhikw5AvcM6YmK2jbwbFnQgQiu1gLR0ymXw1FbB9dw6C';
 
-$consumerKey = 'hPzVCCOxhVXN2nkRljbQa45yTvmkbD0ORiJ0N2uyA8iwJGhwcS';
-$consumerSecret = 'i4V1CBC22FU977dJgydgOcVRIlPCtSuBvre3bGdv41VAKsAXRZ';
-$oauthToken = 'bu9GtwLSMCC4SQckhzWvRvrplHwTCtwFxSR2ytHOB8EEhBAgHQ';
-$oauthTokenSecret = 'rflb0Fbhikw5AvcM6YmK2jbwbFnQgQiu1gLR0ymXw1FbB9dw6C';
+echo "=== Base64 Decoding Check ===\n";
+echo "Raw Token: '{$tok}' (len: " . strlen($tok) . ")\n";
+echo "Base64 Decoded Token: '" . base64_decode($tok) . "'\n\n";
 
-$url = "https://api.tumblr.com/v2/user/info";
-$authHeader = getTumblrOAuthHeader($consumerKey, $consumerSecret, $oauthToken, $oauthTokenSecret, $url, 'GET', []);
-
-$ch = curl_init($url);
-curl_setopt_array($ch, [
-    CURLOPT_RETURNTRANSFER => true,
-    CURLOPT_HTTPHEADER     => [$authHeader],
-    CURLOPT_TIMEOUT        => 10,
-    CURLOPT_SSL_VERIFYPEER => false,
-]);
-$resp = curl_exec($ch);
-$code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-curl_close($ch);
-
-echo "GET /v2/user/info Response Code: HTTP {$code}\n";
-echo "Response Body: {$resp}\n";
+echo "Raw Token Secret: '{$sec}' (len: " . strlen($sec) . ")\n";
+echo "Base64 Decoded Token Secret: '" . base64_decode($sec) . "'\n";
