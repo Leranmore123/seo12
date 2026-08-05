5a8123e3fad637a82431810b97c5a9411243b0a4
kanzariyapratik007 <kanzariyapratik124@gmail.com>
2026-08-06 01:17:14 +0530

    Test real post on active Tumblr blog howtoverifypropertyingujara

--- scratch/test_tumblr_debug.php
+++ scratch/test_tumblr_debug.php
@@ -6,33 +6,18 @@ $consumerKey = 'QRWO9l1KifJyiBJICcvOkHS5J0XM4UT3AzdjRdeWocev3mEb8z';
 $consumerSecret = 'C0lFnqNOncJ5zbCw3fFLjQxpBW4EiefYfWBR6mWOe3Kc7n8XYa';
 $oauthToken = '9vldaudhMnkJt9FkTpDF6MQ6i7d7s86imqAnpylT5zEQBuowP1';
 $oauthTokenSecret = 'l13CukH4Obp1PBWXchWl6cj6qrMEp3l4UvxECdo5LaZ9NFok3r';
+$blogName = 'howtoverifypropertyingujara';
 
-echo "=== Testing Fresh Tumblr Authorized Account ===\n\n";
+echo "=== Testing Real Tumblr Post to {$blogName} ===\n\n";
 
-// Step 1: User Info Test
-$url = "https://api.tumblr.com/v2/user/info";
-$authHeader = getTumblrOAuthHeader($consumerKey, $consumerSecret, $oauthToken, $oauthTokenSecret, $url, 'GET', []);
+$creds = [
+    'api_key'    => $consumerKey,
+    'api_secret' => $consumerSecret,
+    'username'   => $blogName,
+    'password'   => base64_encode($oauthToken . ':' . $oauthTokenSecret)
+];
 
-$ch = curl_init($url);
-curl_setopt_array($ch, [
-    CURLOPT_RETURNTRANSFER => true,
-    CURLOPT_HTTPHEADER     => [$authHeader],
-    CURLOPT_TIMEOUT        => 10,
-    CURLOPT_SSL_VERIFYPEER => false,
-]);
-$resp = curl_exec($ch);
-$code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-curl_close($ch);
+$res = postToTumblr($creds, 'property in Gujarat', 'https://propertysdeal.in/propertys-details/property-in-gujarat', OPENAI_API_KEY, OPENAI_API_KEY, 1, [], 214);
 
-echo "User Info Response Code: HTTP {$code}\n";
-$data = json_decode($resp, true);
-
-if ($code === 200 && isset($data['response']['user']['blogs'])) {
-    echo "🎉 SUCCESS! Connected User: " . $data['response']['user']['name'] . "\n";
-    echo "Available Blogs under this account:\n";
-    foreach ($data['response']['user']['blogs'] as $b) {
-        echo " - " . $b['name'] . " (" . $b['url'] . ")\n";
-    }
-} else {
-    echo "Error response: {$resp}\n";
-}
+echo "Post Result:\n";
+print_r($res);
