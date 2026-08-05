69d63a016ba7c886689d330dc1d6f587e57c3bb6
kanzariyapratik007 <kanzariyapratik124@gmail.com>
2026-08-05 23:00:48 +0530

    Add --disable-gpu to Symbaloo launch args to prevent Chromium GPU crashes

--- selenium/symbaloo_post_playwright.py
+++ selenium/symbaloo_post_playwright.py
@@ -117,6 +117,8 @@ def symbaloo_post(email, password, keyword, target_url, custom_mix_url="", ai_de
                     "--no-sandbox",
                     "--disable-setuid-sandbox",
                     "--disable-dev-shm-usage",
+                    "--disable-gpu",
+                    "--disable-software-rasterizer",
                     "--disable-blink-features=AutomationControlled"
                 ],
                 user_agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36"
